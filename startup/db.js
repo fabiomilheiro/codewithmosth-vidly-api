584cfdf2a147fe4e947cb67128024c9803803ce8
Fabio Milheiro <fabiomilheiro@pikabytes.com>
2020-03-28 16:50:13 +0000

    Removed bug.

--- startup/db.js
+++ startup/db.js
@@ -4,6 +4,5 @@ const config = require("config");
 
 module.exports = function() {
   const db = config.get("db");
-  throw new Error(JSON.stringify(db));
   mongoose.connect(db).then(() => winston.info(`Connected to ${db}...`));
 };
