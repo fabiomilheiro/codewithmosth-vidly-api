f9834a6d97f34c3973f641096499ef222ed8367a
Fabio Milheiro <fabiomilheiro@pikabytes.com>
2020-03-28 17:29:42 +0000

    Added root url handler and db connection string logging.

---
+++ routes/root.js
@@ -0,0 +1,13 @@
+const validateObjectId = require("../middleware/validateObjectId");
+const auth = require("../middleware/auth");
+const admin = require("../middleware/admin");
+const { Genre, validate } = require("../models/genre");
+const mongoose = require("mongoose");
+const express = require("express");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  res.send({ message: "Welcome!" });
+});
+
+module.exports = router;
--- startup/db.js
+++ startup/db.js
@@ -4,5 +4,6 @@ const config = require("config");
 
 module.exports = function() {
   const db = config.get("db");
+  winston.info(`The db connection string: ${db}`);
   mongoose.connect(db).then(() => winston.info(`Connected to ${db}...`));
 };
