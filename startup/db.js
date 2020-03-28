f7a083bbf3819495349394a16027127329cbd93c
Fabio Milheiro <fabiomilheiro@pikabytes.com>
2020-03-28 17:41:01 +0000

    Added db connection options.

--- startup/db.js
+++ startup/db.js
@@ -5,5 +5,7 @@ const config = require("config");
 module.exports = function() {
   const db = config.get("db");
   winston.info(`The db connection string: ${db}`);
-  mongoose.connect(db).then(() => winston.info(`Connected to ${db}...`));
+  mongoose
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => winston.info(`Connected to ${db}...`));
 };
